6f1f88268a8f5ed502b18d2209c102b2c8f7e3ed
Magestan <88542209+Magestan@users.noreply.github.com>
2023-03-09 23:06:52 +0000

    testchange1

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const secondHand = document.querySelector('.second-hand');
   function setDate() {
     const now = new Date();
     const seconds = now.getSeconds();
-    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    const secondsDegrees = ((seconds / 30) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
     const mins = now.getMinutes();
     const minsDegrees = ((mins / 60) * 360) + ((seconds/60)*6) + 90;
